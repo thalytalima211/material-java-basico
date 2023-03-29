e9791e72789e60ea975de352a51cb3c2beea1764
thalytalima211 <thalytalima211@gmail.com>
2023-03-29 14:29:48 -0300

    adiciona condicaoTernaria

---
+++ tipos-variaveis/src/condicaoTernaria.java
@@ -0,0 +1,8 @@
+public class condicaoTernaria {
+    public static void main(String[] args) throws Exception {
+        int a=5;
+        int b=5;
+        String condicao = a==b ? "Verdadeiro" : "Falso";
+        System.out.println(condicao);
+    }
+}
