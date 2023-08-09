68556a5df47bad2377379bfcd28b423f5babd5f5
Thalyta Lima <thalytalima211@gmail.com>
2023-08-09 16:28:25 -0300

    Teste em atualização do gitignore

--- laço-e-array/src/Exercicio1_array.java
+++ laço-e-array/src/Exercicio1_array.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercicio1_array {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Digite a quantidade de elementos desejada no vetor: ");
+        System.out.print("Digite a quantidade de elementos desejada no vetor: ");lei
         int comprimento = scan.nextInt();
         int[] vetor = new int[comprimento];
 
